a188068d8d3e8fc613a73757019f8b09cbee6363
Ioannis "Yiannis" Kontinopoulos <44923054+jenkei@users.noreply.github.com>
2019-05-21 14:22:56 +0300

    Create Exercise.cs

---
+++ Week 2/Day 1/Exercise.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace FullName
+{
+    class TakeData
+    {
+       static void Main()
+        {
+            // Ask for first name
+            Console.WriteLine("Please provide your first name");
+
+            string FirstName = Console.ReadLine();
+           
+            // Ask for last name
+            Console.WriteLine("Now, please provide your last name");
+
+            // Store last name
+            string LastName = Console.ReadLine();
+
+            // Ask for address
+            Console.WriteLine("Now, please provide your city of residence");
+
+            // Store city
+            string City = Console.ReadLine();
+
+            // Print out message
+            Console.WriteLine("Hello " + FirstName + " " + LastName+"!");
+            Console.WriteLine("You live in " + City+"!");
+            
+        }
+    }
+}
