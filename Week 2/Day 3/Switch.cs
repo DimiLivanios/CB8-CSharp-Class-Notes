bde5aaeac6f72f580751f1238131aa62c12a8cc3
Ioannis "Yiannis" Kontinopoulos <44923054+jenkei@users.noreply.github.com>
2019-05-22 10:35:42 +0300

    Create Switch.cs

---
+++ Week 2/Day 3/Switch.cs
@@ -0,0 +1,78 @@
+using System;
+namespace Day3
+{
+    class Switch
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert a number from 1-7");
+            int number = int.Parse(Console.ReadLine());
+
+            /* To avoid repeating if (number == 1)
+             * (...)           
+             * else if(number == 2)
+             * (...)
+             * .
+             * .
+             * .
+             * We can write
+             */
+
+             switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break; /* We need breaks because we do not use curly brackets
+                            * breaks completely stop the switch and get out of it
+                            */
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sudnay");
+                    break;
+                default:
+                    Console.WriteLine("Wrong input");
+                    break;
+            }
+
+            /* To avoid repeating if (number == 1 || number == 2 || ...)
+             * (...)
+             * else if (number == 6 || number == 7)
+             *
+             * We can write 
+             */
+
+            switch (number)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Week");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine("Weekend");
+                    break;
+                default:
+                    Console.WriteLine("Wrong input");
+                    break;
+            }
+                    //Console.Readkey()
+        }
+    }
+}
