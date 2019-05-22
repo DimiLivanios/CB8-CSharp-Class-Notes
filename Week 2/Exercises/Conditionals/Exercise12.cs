2b405509e7cbe204f6b0020cf08e0d3c43d11cbb
Ioannis "Yiannis" Kontinopoulos <44923054+jenkei@users.noreply.github.com>
2019-05-22 15:01:49 +0300

    Update Exercise12.cs

--- Week 2/Exercises/Conditionals/Exercise12.cs
+++ Week 2/Exercises/Conditionals/Exercise12.cs
@@ -13,53 +13,47 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the first Integer");
-            double first = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the second Integer");
-            double second = double.Parse(Console.ReadLine());
-            Console.WriteLine("Here are the options:");
-            for (int i = 1; i <= 5; i++)
+            string answer = "y";
+            do
             {
-                Exercise12Options Choices = (Exercise12Options)i;
-                switch (Choices)
+                Console.WriteLine("Enter the first Integer");
+                double first = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second Integer");
+                double second = double.Parse(Console.ReadLine());
+                Console.WriteLine("Here are the options:");
+                string[,] array = { { "1", "Addition" }, { "2", "Subtraction" }, { "3", "Multiplication" }, { "4", "Division" }, { "5", "Exit" } };
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    Console.WriteLine($"{ array[i,0]} - { array[i,1]}");
+                }
+                Console.WriteLine("Input your choice");
+                int Choice = int.Parse(Console.ReadLine());
+                Exercise12Options SelectionOutput = (Exercise12Options)Choice;
+                switch (SelectionOutput)
                 {
                     case Exercise12Options.Addition:
-                        Console.WriteLine("1 - Addition");
+                        Console.WriteLine(first + second);
                         break;
                     case Exercise12Options.Subtraction:
-                        Console.WriteLine("2 - Subtraction");
+                        Console.WriteLine(first - second);
                         break;
                     case Exercise12Options.Multiplication:
-                        Console.WriteLine("3 - Multiplication");
+                        Console.WriteLine(first * second);
                         break;
                     case Exercise12Options.Division:
-                        Console.WriteLine("4 - Division");
+                        Console.WriteLine(first / second);
                         break;
                     case Exercise12Options.Exit:
-                        Console.WriteLine("5 - Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input, try again");
                         break;
                 }
-            }
-            Console.WriteLine("Input your choice");
-            int Choice = int.Parse(Console.ReadLine());
-            Exercise12Options SelectionOutput = (Exercise12Options)Choice;
-            switch (SelectionOutput)
-            {
-                case Exercise12Options.Addition:
-                    Console.WriteLine(first + second);
-                    break;
-                case Exercise12Options.Subtraction:
-                    Console.WriteLine(first - second);
-                    break;
-                case Exercise12Options.Multiplication:
-                    Console.WriteLine(first * second);
-                    break;
-                case Exercise12Options.Division:
-                    Console.WriteLine(first / second);
-                    break;
-                case Exercise12Options.Exit:
-                    break;
-            }
+                Console.WriteLine("Want to do another calculation? (y/n)");
+                answer = Console.ReadLine();
+            } while (answer == "y");
+
+            //Console.ReadLine();
         }
     }
 }
