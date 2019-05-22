57c057bc113764bb67a43a3b592f29d702941831
Ioannis "Yiannis" Kontinopoulos <44923054+jenkei@users.noreply.github.com>
2019-05-22 10:38:21 +0300

    Add files via upload

---
+++ Week 2/Day 3/Enums.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace Enums
+{
+/* There is a data type called enum which allows us to allocate an item/object 
+ * to a corresponding integer. We define it outside our class type because it 
+ * is a type of its own. So instead of writing
+ * 
+ * const Monday = 1
+ * const Tuesday = 2
+ * .
+ * .
+ * .
+ * 
+ * We write 
+ */
+    enum DaysOfWeek
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+/* This allows us to easily identify the cases in switch as shown in the example
+ * below
+ */
+    class Week
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert a number from 1-7");
+            int number = int.Parse(Console.ReadLine());
+
+            // Here we cast the number as an object of DaysOfWeek
+            DaysOfWeek d = (DaysOfWeek)number;
+
+            switch (d)
+            {
+                case DaysOfWeek.Monday:
+                case DaysOfWeek.Tuesday:
+                case DaysOfWeek.Wednesday:
+                case DaysOfWeek.Thursday:
+                case DaysOfWeek.Friday:
+                    Console.WriteLine("Weekday");
+                    break;
+                case DaysOfWeek.Saturday:
+                case DaysOfWeek.Sunday:
+                    Console.WriteLine("Weekend");
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input");
+                    break;
+            }
+        }
+    }
+}
---
+++ Week 2/Day 3/MoreLoops.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Day3
+{
+    public class MoreLoops
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                // Continue makes us go to the next iteration of the loop
+                if (i % 2 != 0)
+                    continue;
+
+                /* Break will stop the iteration. But given we have reached 
+                 * continue above the code below is skipped
+                 */
+                if (i == 51)
+                    break;
+                Console.WriteLine(i);
+            }
+
+            // This will never run
+            while (false)
+            {
+
+            }
+
+            // This will always run, forever
+            while (true)
+            {
+
+            }
+        }
+    }
+}
