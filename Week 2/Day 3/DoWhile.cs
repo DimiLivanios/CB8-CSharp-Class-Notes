b34ac5a7b1029e2fa02169e99d4becf966d08fbc
Ioannis "Yiannis" Kontinopoulos <44923054+jenkei@users.noreply.github.com>
2019-05-22 11:30:10 +0300

    Add files via upload

---
+++ Week 2/Day 3/DoWhile.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace Day3
+{
+    class DoWhile
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Pick a number (1-6): ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //Random dice = new Random();
+            //if (dice.Next(1, 7) == number)
+            //    Console.WriteLine("You win");
+            //else
+            //    Console.WriteLine("You lose");
+
+            //Console.WriteLine("Want to play again? (y/n)");
+            //string answer = Console.ReadLine();
+
+            //while (answer == "y") ;
+            //{
+            //    Console.WriteLine("Pick a number (1-6): ");
+            //    number = int.Parse(Console.ReadLine());
+            //    if (dice.Next(1, 7) == number)
+            //        Console.WriteLine("You win");
+            //    else
+            //        Console.WriteLine("You lose");
+            //    Console.WriteLine("Want to play again? (y/n)");
+            //    answer = Console.ReadLine();
+            //}
+
+            /* As you will notice above, we have repetition of code. We can instead
+             * initialize an answer and avoid repetition
+             */
+
+            string answer = "n";
+            while (answer == "y")
+            {
+                Console.WriteLine("Pick a number (1-6): ");
+                int number = int.Parse(Console.ReadLine());
+
+                Random dice = new Random();
+                if (dice.Next(1, 7) == number)
+                    Console.WriteLine("You win");
+                else
+                    Console.WriteLine("You lose");
+
+                Console.WriteLine("Want to play again? (y/n)");
+                answer = Console.ReadLine();
+            }
+
+            /* However, the problem with while is that it depends on initializing
+             * a condition. If we make a mistake and initialize answer = "n", the
+             * loop will never start. If we want to run at least once, we use do-
+             * -while
+             */
+
+            do
+            {
+                Console.WriteLine("Pick a number (1-6): ");
+
+                /* Note that we have a second initialization of the variable
+                 * number. This is because the variable is local within the block
+                 */
+                int number = int.Parse(Console.ReadLine());
+
+                Random dice = new Random();
+                if (dice.Next(1, 7) == number)
+                    Console.WriteLine("You win");
+                else
+                    Console.WriteLine("You lose");
+
+                Console.WriteLine("Want to play again? (y/n)");
+
+                /* Note that answer needs to be initialized outside of the loop.
+                 * if that was not the case, the line below would not work.
+                 */
+                answer = Console.ReadLine();
+            } while (answer == "y");
+            /* We can instead write while (Console.ReadLine() == "y") and delete
+             * the assignment of answer within the block   
+             */
+
+            //Console.ReadKey()
+        }
+    }
+}
