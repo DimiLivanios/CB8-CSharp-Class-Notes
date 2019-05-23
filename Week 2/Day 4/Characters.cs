23b7c1922352c9870e11f8cc83fe61d58973cc7d
Ioannis "Yiannis" Kontinopoulos <44923054+jenkei@users.noreply.github.com>
2019-05-23 09:41:43 +0300

    Update and rename Characters to Characters.cs

--- Week 2/Day 4/Characters.cs
+++ Week 2/Day 4/Characters.cs
@@ -13,6 +13,13 @@ namespace Day4
 
             // Likewise, we can cast an integer into a character
             Console.WriteLine((char)97); // Prints out 'a'.
+
+            // \n changes line (note the forward slash)
+            Console.WriteLine("\n Hello, World! \n";
+            /* The forward slash in this case is called an escape character.
+             * An escape character modifies a following string or character's
+             * function (in this case n is not 'n' but a line change).
+             */            
         }
     }
 }
