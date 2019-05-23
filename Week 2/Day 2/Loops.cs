983fd19bbdaab5cca6ff333199ea19e7428e44fe
Ioannis "Yiannis" Kontinopoulos <44923054+jenkei@users.noreply.github.com>
2019-05-23 14:37:09 +0300

    Update Loops.cs

--- Week 2/Day 2/Loops.cs
+++ Week 2/Day 2/Loops.cs
@@ -1,90 +1,22 @@
 using System;
-namespace Day2
+namespace Day4
 {
-    class Loops
+    class MathOperations
     {
         static void Main(string[] args)
         {
-            Console.Write("Insert a number: ");
-            // Cannot assign string to int
-            int number = Console.ReadLine(); // Produces error
+            /* There is a useful library in .NET called Math. It contains useful
+             * objects like pi
+             */
+            double pi = Math.PI;
+            Console.WriteLine(pi);
 
-            // Two ways to input string as number
-            int number1 = Convert.ToInt32(Console.ReadLine());
-            int number2 = int.Parse(Console.ReadLine());
+            // Float in .NET is an alias for a data type called Single
+            string String = "3.14";
+            float pi2 = float.Parse(String);
+            float pi3 = Convert.ToSingle(String);
 
-            int number1 = int.Parse(Console.ReadLine());
-            
-            // Assign a new Random() method to the rand variable using the Random class
-            Random rand = new Random();
-
-            // Different ways to add to a variable
-            int a = 1;
-            a = a + 1;
-
-            Console.WriteLine(a);
-
-            a += 1; // (Same thing)
-
-            Console.WriteLine(a);
-
-            a++; // (Same thing)
-
-            Console.WriteLine(a);
-
-            // And, similarly
-            a--;
-
-            Console.WriteLine(a);
-
-            // Note, however, that
-            Console.WriteLine(a++); // Prints out a, since it evaluates the expression from left to right
-
-            /* Here, the process followed is
-             * Console.WriteLine(a);
-             * a++; */            
-
-
-            // To solve for this, we write
-            Console.WriteLine(++a);
-
-            /* Where the process followed is
-             * a++;     
-             * Console.WriteLine(a); */
-
-            // Program-defined loop iterations
-            for (int i = 0; i < 10; i++)
-            {
-                if (number1 == rand.Next(1, 7))
-                    Console.WriteLine("You win!");
-                else
-                    Console.WriteLine("You lose!");
-            }
-
-            // User-defined loop iterations
-            Console.WriteLine("How many times do you wanna play?");
-            int times = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < times; i++)
-            {
-                if (number1 == rand.Next(1, 7))
-                    Console.WriteLine("You win!");
-                else
-                    Console.WriteLine("You lose!");
-            }
-
-            // Undefined loop iterations (Play till you win)
-            Console.WriteLine("Please insert a number");
-            int number2 = int.Parse(Console.ReadLine());
-            while (number2 != rand.Next(1, 7)) 
-            {
-                Console.WriteLine("You lose!");
-                Console.WriteLine("Insert another number");
-                number2 = int.Parse(Console.ReadLine());
-            }
-            Console.WriteLine("You win!");
-            
-            //Console.Readkey()
+            //Console.ReadKey();
         }
     }
 }
