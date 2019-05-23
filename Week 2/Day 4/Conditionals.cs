70c71ad7264ff2b086b8dfdb6c444c340535a76c
Ioannis "Yiannis" Kontinopoulos <44923054+jenkei@users.noreply.github.com>
2019-05-23 10:11:47 +0300

    Add files via upload

---
+++ Week 2/Day 4/Conditionals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Day4
+{
+    class Conditionals
+    {
+        static void Main(string[] args)
+        {
+            /* If we have two cases for a conditional, we can use a ternary
+             * operator
+             */
+            int int1 = 1;
+            int int2 = 2;
+            string message = int1 == int2
+                ? $"{int1} and {int2} are equal" /* ? is a ternary operator.
+                                                  * Translates to "then" (implication
+                                                  * to the condition int1 == int2
+                                                  */
+                : $"{int1} and {int2} are not equal."; // : translates to "otherwise"
+          }
+    }
+}
---
+++ Week 2/Day 4/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Day4
+{
+    class MathOperations
+    {
+        static void Main(string[] args)
+        {
+            /* There is a useful library in .NET called Math. It contains useful
+             * objects like pi
+             */
+            double pi = Math.PI;
+            Console.WriteLine(pi);
+
+            // Float in .NET is an alias for a data type called Single
+            string String = "3.14";
+            float pi2 = float.Parse(String);
+            float pi3 = Convert.ToSingle(String);
+
+        }
+    }
+}
