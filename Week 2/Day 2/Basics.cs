61768bc30b0a0bc371130c35455198b56404dc14
Ioannis "Yiannis" Kontinopoulos <44923054+jenkei@users.noreply.github.com>
2019-05-23 14:36:03 +0300

    Update Basics.cs

--- Week 2/Day 2/Basics.cs
+++ Week 2/Day 2/Basics.cs
@@ -1,11 +1,11 @@
 using System;
-namespace Day2
+namespace ClassExamples
 {
     class Basics
     {
         static void Main(string[] args)
         {
-            // Have to declare data type. Can declare many variables' types in same function
+ // Have to declare data type. Can declare many variables' types in same function
             int Number = 0, Number1 = 1;
 
             Console.WriteLine(Number);
@@ -21,13 +21,13 @@ namespace Day2
 
             Console.WriteLine(Number2);
 
-            // C# considers decimals as doubles, so to declare a float we put an f after the number
+ // C# considers decimals as doubles, so to declare a float we put an f after the number
             float Pi = 3.14; // Produces error
             float Pi = 3.14f; // This is called casting
 
             Console.WriteLine(Pi);
 
-            // The output for arithmetic operations is of the same type as the inputs
+       // The output for arithmetic operations is of the same type as the inputs
             int a = 5, b = 10;
              
             Console.WriteLine(a / b);
@@ -38,20 +38,21 @@ namespace Day2
 
             Console.WriteLine(c / d);
 
-            // We can explicitly cast a variable and the output below turns into a float
+    // We can explicitly cast a variable and the output below turns into a float
             Console.WriteLine(a / (float)b);
 
             // Cannot implicitly cast a double into a float
             double e = 12.3;
-            float f = e; // Produces error. Need to explicitly cast if we want to take the risk.
+            float f = e; 
+         // Produces error. Need to explicitly cast if we want to take the risk.
 
             // WriteLine changes line, while Write stays on the same
             Console.Write("Hello");
             Console.WriteLine(",");
             Console.WriteLine("World");
             Console.Write("!");
-            
-            //Console.ReadKey()
+
+            //Console.ReadKey();
         }
     }
 }
