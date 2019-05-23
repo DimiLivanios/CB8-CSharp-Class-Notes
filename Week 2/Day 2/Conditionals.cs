b3ee807c8224cb2e1b9cb2b89dcaf937787ccac3
Ioannis "Yiannis" Kontinopoulos <44923054+jenkei@users.noreply.github.com>
2019-05-23 14:36:23 +0300

    Update Conditionals.cs

--- Week 2/Day 2/Conditionals.cs
+++ Week 2/Day 2/Conditionals.cs
@@ -1,5 +1,5 @@
 using System;
-namespace Day2
+namespace ClassExamples
 {
     class Conditionals
     {
@@ -11,19 +11,20 @@ namespace Day2
             bool b = true;
             bool c = false;
 
-            if(name == "Computer")
+            if (name == "Computer")
                 // One line of code in conditional doesn't need curly brackets
                 Console.WriteLine("We have the same name!");
             else
-                // Multiple lines of code need curly brackets to group conditional block
+        // Multiple lines of code need curly brackets to group conditional block
             {
                 Console.WriteLine("Hello, " + name);
                 // Two additional ways to write in console
                 Console.WriteLine("What's up, {0} {1}?", name, "(Surname)");
-                Console.WriteLine($"Your name is {name} {"(Surname)"}"); // This is called string interpolation
-                
-                //Console.ReadKey()
+                Console.WriteLine($"Your name is {name} {"(Surname)"}");
+                // This is called string interpolation
             }
+
+            //Console.ReadKey();
         }
     }
 }
